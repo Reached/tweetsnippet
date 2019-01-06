9c9b15648e24adebf71b85e24a88af4113646eed
Casper Sørensen <casper@ulobby.dk>
2019-01-06 10:39:23 +0200

    Seed the database with a couple of tags to get started with

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Tag;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Tag::create([
+            'name' => 'Laravel',
+            'slug' => 'laravel'
+        ]);
+
+        Tag::create([
+            'name' => 'Javascript',
+            'slug' => 'javascript'
+        ]);
     }
 }

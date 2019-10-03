a6374de5ebf9628e09fa3491113fd9faf7f7fd8f
Niels Kootstra <niels.kootstra@gmail.com>
2019-10-03 10:50:35 +0200

    Fixed the search functionality by checking if there are any contributors.

--- resources/views/layouts/includes/sidebar.blade.php
+++ resources/views/layouts/includes/sidebar.blade.php
@@ -1,10 +1,14 @@
-<aside class="sidebar card flex flex-column box-shadow">
+<aside class="sidebar card box-shadow mr-4">
     <h3 class="header">Top contributors</h3>
     <div class="p-1">
-        @foreach($contributors as $contributor)
-            @foreach($contributor as $c)
-                <li><span class="count">{{ $c['count'] }}</span><a href="{{ 'https://twitter.com/' . $c['handle'] }}">{{ $c['handle'] }}</a></li>
+        @if(isset($contributors))
+            @foreach($contributors as $contributor)
+                @foreach($contributor as $c)
+                    <li><span class="count">{{ $c['count'] }}</span><a href="{{ 'https://twitter.com/' . $c['handle'] }}">{{ $c['handle'] }}</a></li>
+                @endforeach
             @endforeach
-        @endforeach
+        @else
+            <div class="pt-3 text-center">No contributors found.</div>
+        @endif
     </div>
 </aside>

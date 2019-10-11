3085447fa64e71d674cc70faaad9ce6101c7c01a
Casper Sørensen <casper@ulobby.dk>
2019-10-11 09:42:05 +0200

    removed sidebar

--- resources/views/snippets/search/show.blade.php
+++ resources/views/snippets/search/show.blade.php
@@ -12,16 +12,13 @@
 
 <h1 class="headline-centered">Snippets matching <strong>{{ $query }}</strong></h1>
 
-<div class="upper-grid">
-    <div class="grid {{ ($snippets->count() === 0) ? 'no-items' : '' }}">
-        @forelse($snippets as $snippet)
-            @include('snippets.includes.snippet')
-        @empty
-            <h3 class="headline-centered">We found no snippets matching your query, <a href="#" class="search-button">try a new one</a></h3>
-            {{--<a href="{{ route('contribute') }}" class="centered-link">You can contribute here</a>--}}
-        @endforelse
-    </div>
-    @include('layouts.includes.sidebar')
+<div class="grid">
+    @forelse($snippets as $snippet)
+        @include('snippets.includes.snippet')
+    @empty
+        <h3 class="headline-centered">We found no snippets matching your query, <a href="#" class="search-button">try a new one</a></h3>
+        {{--<a href="{{ route('contribute') }}" class="centered-link">You can contribute here</a>--}}
+    @endforelse
 </div>
 
 {{ $snippets->links() }}
